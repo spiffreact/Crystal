a00c65bab19a30fc6661ce51cdf9607b2914d80d
Jin <gogomose@gmail.com>
2025-07-22 21:31:02 +0900

    feat: dropdown menu mobile ver fixed again6

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -53,7 +53,7 @@ export default function Footer() {
                   <path d="M16.273 12.845L7.376 0H0v24h7.727V11.155L16.624 24H24V0h-7.727v12.845z"/>
                 </svg>
               </a>
-              <a 
+              {/* <a 
                 href="" 
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -63,7 +63,7 @@ export default function Footer() {
                 <svg width="24" height="24" viewBox="0 0 24 24" fill="currentColor">
                   <path d="M16.273 12.845L7.376 0H0v24h7.727V11.155L16.624 24H24V0h-7.727v12.845z"/>
                 </svg>
-              </a>
+              </a> */}
             </div>
           </div>
           <div className={styles.copyright}>
